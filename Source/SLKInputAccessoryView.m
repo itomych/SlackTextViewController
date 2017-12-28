3b16fbd2e80c708c0e7c139a999b705f960359e4
Yevhen Staroselskyi <evgeniys@itomy.ch>
2017-12-28 17:43:26 +0200

    fixed issue with swipe down to hide keyboard feature

--- Source/SLKInputAccessoryView.m
+++ Source/SLKInputAccessoryView.m
@@ -14,10 +14,25 @@
 
 #pragma mark - Super Overrides
 
-- (void)willMoveToSuperview:(UIView *)newSuperview
-{
-    if (!SLK_IS_IOS9_AND_HIGHER) {
-        _keyboardViewProxy = newSuperview;
+- (void)willMoveToSuperview:(UIView *)newSuperview {
+    if (newSuperview) {
+        if (SLK_IS_IOS9_AND_HIGHER) {
+            
+            NSPredicate *windowPredicate = [NSPredicate predicateWithFormat:@"self isMemberOfClass: %@", NSClassFromString(@"UIRemoteKeyboardWindow")];
+            UIWindow *keyboardWindow = [[[UIApplication sharedApplication].windows filteredArrayUsingPredicate:windowPredicate] firstObject];
+            
+            for (UIView *subview in keyboardWindow.subviews) {
+                for (UIView *hostview in subview.subviews) {
+                    if ([hostview isMemberOfClass:NSClassFromString(@"UIInputSetHostView")]) {
+                        _keyboardViewProxy = hostview;
+                        break;
+                    }
+                }
+            }
+        }
+        else {
+            _keyboardViewProxy = newSuperview;
+        }
     }
 }
 
